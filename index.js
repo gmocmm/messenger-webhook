065ec45db31ca5025f5f6b55da64da143f6ebda2
Guillermo Camarena Miramontes <gmocmm@MacBook-Pro-de-Guillermo.local>
2021-05-26 15:44:52 -0500

    Test

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json()); // creates express http server
+const request = require('request');
+require('dotenv').config();
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -18,7 +23,19 @@ app.post('/webhook', (req, res) => {
     // Gets the message. entry.messaging is an array, but 
     // will only ever contain one message, so we get index 0
     let webhook_event = entry.messaging[0];
-    console.log(webhook_event, 'ok');
+    console.log(webhook_event);
+
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+
+    // Check if the event is a message or postback and
+    // pass the event to the appropriate handler function
+    if (webhook_event.message) {
+      handleMessage(sender_psid, webhook_event.message);        
+    } else if (webhook_event.postback) {
+      handlePostback(sender_psid, webhook_event.postback);
+    }
   });
 
     // Returns a '200 OK' response to all requests
@@ -33,7 +50,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let VERIFY_TOKEN = VERIFY_TOKEN;
     
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -57,6 +74,53 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
 
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
+
+// Sets server port and logs message on success
+const port = process.env.PORT || 1337;
+app.listen(port, () => console.log(`Webhook is listening at port ${port}`));
