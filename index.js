f3702cc9320afa55da99b5c61d9184ccc6bfc123
Guillermo Camarena Miramontes <gmocmm@MacBook-Pro-de-Guillermo.local>
2021-05-29 14:01:01 -0500

    test

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express().use(bodyParser.json()); // creates express http server
 const request = require('request');
 require('dotenv').config();
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 app.get('/webhook', (req, res) => {
--- utils/call-send-api.js
+++ utils/call-send-api.js
@@ -1,3 +1,5 @@
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 const callSendApi = (request_body) => {
   request({
     uri: "https://graph.facebook.com/v10.0/me/messages",
