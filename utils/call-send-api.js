a34780ab5cc6e678ec1b572f811fc6747a3d65c2
Guillermo Camarena Miramontes <gmocmm@MacBook-Pro-de-Guillermo.local>
2021-05-29 14:26:59 -0500

    test

--- utils/call-send-api.js
+++ utils/call-send-api.js
@@ -2,7 +2,7 @@ const request = require('request');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const callSendApi = (request_body) => {
-  console.log(request_body);
+  console.log(request_body, PAGE_ACCESS_TOKEN);
   
   request({
     uri: "https://graph.facebook.com/v10.0/me/messages",
