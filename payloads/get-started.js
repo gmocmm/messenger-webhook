2b65a292269ef45ded711f6cf5234b4faefb030a
Guillermo Camarena Miramontes <gmocmm@MacBook-Pro-de-Guillermo.local>
2021-05-29 17:08:27 -0500

    test

--- payloads/get-started.js
+++ payloads/get-started.js
@@ -4,6 +4,7 @@ const { CALL_SEND_API } = require('./../utils/call-send-api');
 const handleGetStartedPayload = (sender_psid) => {
   // Welcome Message
   SET_SENDER_ACTION(sender_psid, 'typing_on');
+
   setTimeout(() => {
     CALL_SEND_API({
       "recipient": {
@@ -12,10 +13,12 @@ const handleGetStartedPayload = (sender_psid) => {
       "message": { "text": "¡Hola {{name}}! Soy Bot Jr. 🤖 y me encantan las hamburguesas 🍔 como a ti." },
       "typing_off": "typing_off"
     });
+
+    SET_SENDER_ACTION(sender_psid, 'typing_on');
   }, 1000);
 
   // Are you ready? Message
-  SET_SENDER_ACTION(sender_psid, 'typing_on');
+  
   setTimeout(() => {
     CALL_SEND_API({
       "recipient": {
@@ -24,10 +27,11 @@ const handleGetStartedPayload = (sender_psid) => {
       "message": { "text": "¿Listo? Estoy aquí para ayudarte." },
       "typing_off": "typing_off"
     });
-  }, 1000);
+
+    SET_SENDER_ACTION(sender_psid, 'typing_on');
+  }, 2000);
 
   // Instrucion Message 
-  SET_SENDER_ACTION(sender_psid, 'typing_on');
   setTimeout(() => {
     CALL_SEND_API({
       "recipient": {
@@ -36,7 +40,7 @@ const handleGetStartedPayload = (sender_psid) => {
       "message": { "text": "Selecciona una opción. 🤓" },
       "typing_off": "typing_off"
     });
-  }, 1000);
+  }, 3000);
 };
 
 module.exports = {
