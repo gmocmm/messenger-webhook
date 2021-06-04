94b974a1085c2c9d6ca58ee30bdff82e8e64f6d7
Speed-142 <speed-142@iMac-de-Speed-142.local>
2021-06-04 13:03:39 -0500

    Temp

--- payloads/getStartedPayload.js
+++ payloads/getStartedPayload.js
@@ -4,20 +4,49 @@ const getStartedPayloadHandler = async (sender_psid) => {
   let userData = await GET_USER_DATA(sender_psid);
   userData = JSON.parse(userData);
 
+  // ************************ */
+
+  // Typing On
+  await SEND_REQUEST({
+    "recipient": { "id": sender_psid },
+    "sender_action": "typing_on"  
+  }); 
+  
+  // Message
   await SEND_REQUEST({
     "recipient": { "id": sender_psid },
     "message": { "text": `¡Hola ${userData.first_name}! Soy Bot Jr. 🤖 y me encantan las hamburguesas 🍔 como a ti.` },
   });
 
+  // ************************ */
+
+  // Typing On
+  await SEND_REQUEST({
+    "recipient": { "id": sender_psid },
+    "sender_action": "typing_on"  
+  }); 
+  
+  // Message
   await SEND_REQUEST({
     "recipient": { "id": sender_psid },
     "message": { "text": "¿Listo? Estoy aquí para ayudarte." },
   });
 
+  // ************************ */
+
+  // Typing On
+  await SEND_REQUEST({
+    "recipient": { "id": sender_psid },
+    "sender_action": "typing_on"  
+  }); 
+
+  // Message
   await SEND_REQUEST({
     "recipient": { "id": sender_psid },
     "message": { "text": "Selecciona una opción. 🤓" },
   });
+
+  // ************************ */
 };
 
 module.exports = {
--- utils/callGraphApi.js
+++ utils/callGraphApi.js
@@ -4,7 +4,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const GRAPH_FACEBOOK_URI = 'https://graph.facebook.com/v10.0';
 
 const sendRequest = async (request_body) => {
-  await delay(5000);
+  if(!request_body.sender_action) await delay(5000);
   return new Promise((resolve, reject) => {
     request({
       uri: `${GRAPH_FACEBOOK_URI}/me/messages`,
