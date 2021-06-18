a5631e4df7c84414402f4490e334a3771d3adf0e
Guillermo Camarena <gcamarena@121corp.com>
2021-06-18 17:20:41 -0500

    Temp

--- payloads/sendDisagreementPayload.js
+++ payloads/sendDisagreementPayload.js
@@ -76,7 +76,7 @@ const sendDisagreementPayloadHandler = async (sender_psid, session, received_mes
       });
       
       // Api request to save data
-      console.log(`Request: ${aux_session}`);
+      console.log(`Request: ${aux_session.context}`);
 
       resolve({});
     }
